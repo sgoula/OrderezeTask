e68928b882c69217cb674c2bb5d4614f9ff9c498
sgoula <goula_st88@yahoo.gr>
2016-08-30 01:05:42 +0300

    Add files via upload

---
+++ OrderezeTask/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OrderezeTask.MvcApplication" Language="C#" %>
---
+++ OrderezeTask/Models/ImageContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OrderezeTask.Models
+{
+    public class ImageContext:DbContext
+    {
+        public DbSet<Image> Images { get; set; }
+    }
+}
