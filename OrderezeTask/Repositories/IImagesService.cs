59349acee361719f8f2bb802b607bb149ae39ef0
sgoula <goula_st88@yahoo.gr>
2016-08-31 01:00:52 +0300

    Add files via upload

---
+++ OrderezeTask/Repositories/IImagesService.cs
@@ -0,0 +1,33 @@
+﻿using OrderezeTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderezeTask.Repositories
+{
+    interface IImagesService
+    {
+        /// <summary>
+        /// Returns all images 
+        /// </summary>
+        /// <returns></returns>
+        List<Image> GetImages();
+
+        // Returns Image with given id
+        Image GetImageInfo(int id);
+
+        /// <summary>
+        /// Adds the supplied <paramref name="image"/> to the system and returns the Id.
+        /// Part of the operation is to store the Image in the blob storage.
+        /// </summary>
+        int AddNewImage(Image image);
+
+        /// <summary>
+        /// Deletes the Image with the supplied <paramref name="id"/> from the system 
+        /// and deletes the file from the blob storage as well.
+        /// </summary>
+        void DeleteImage(int id);
+    }
+}
---
+++ OrderezeTask/Repositories/ImageRepository.cs
@@ -0,0 +1,41 @@
+﻿using OrderezeTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrderezeTask.Repositories
+{
+   // Implements operations of IImageService interface
+
+    public class ImageRepository : IImagesService
+    {
+        //Connection to database
+        ImageContext db = new ImageContext();
+
+        public List<Image> GetImages()
+        {
+            return db.Images.ToList();
+        }
+
+        public Image GetImageInfo(int id)
+        {
+            Image ImageInfo = db.Images.Find(id);
+            return db.Images.Find(id);
+        }
+
+        public int AddNewImage(Image image)
+        {
+            db.Images.Add(image);
+            db.SaveChanges();
+            return image.Id;
+        }
+
+        public void DeleteImage(int id)
+        {
+            Image image = db.Images.Find(id);
+            db.Images.Remove(image);
+            db.SaveChanges();
+        }
+    }
+}
