c39d8aed749e0f1fdfa679b5e7aea82aec26922b
sgoula <goula_st88@yahoo.gr>
2016-08-31 21:07:24 +0300

    Add files via upload

---
+++ OrderezeTask/BlobService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrderezeTask
+{
+    public class BlobService
+    {
+
+        public CloudBlobContainer GetCloudBlobContainer()
+        {
+            //Create Account Blob Storage (Windows Authentication)
+            CloudStorageAccount StorageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnStr"));
+            //Create Cloud blob service client
+            CloudBlobClient BlobClient = StorageAccount.CreateCloudBlobClient();
+            //Reference to a created container. Container is created to hold Blobs
+            CloudBlobContainer BlobContainer = BlobClient.GetContainerReference("ImagesBlob");
+            //If container is not exist, create one and set all the required public access permissions.
+            if (BlobContainer.CreateIfNotExists())
+            {
+                BlobContainer.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+            }
+            return BlobContainer;
+        }
+
+        //Upload image to azure blob storage and return block blob
+        public CloudBlockBlob UploadBlob(HttpPostedFileBase Image)
+        {
+            //if image is not null
+            if (Image.ContentLength > 0)
+            {
+                //Return Blob Container
+                CloudBlobContainer BlobContainer = GetCloudBlobContainer();
+                //Reference to blob
+                CloudBlockBlob BlockBlob = BlobContainer.GetBlockBlobReference(Image.FileName);
+
+                try
+                {
+                    BlockBlob.UploadFromStream(Image.InputStream);
+                    return BlockBlob;
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        //Delete Block Blob
+        public void DeleteBlob(string BlobImageName)
+        {
+            CloudBlobContainer BlobContainer = GetCloudBlobContainer();
+            CloudBlockBlob BlockBlob = BlobContainer.GetBlockBlobReference(BlobImageName);
+            BlockBlob.Delete();
+        }
+
+    }
+}
