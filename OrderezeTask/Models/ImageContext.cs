d8f32f4f276e380c05e0a0bd8de26d306f8ce6a1
sgoula <goula_st88@yahoo.gr>
2016-09-05 19:23:42 +0300

    Add files via upload

--- OrderezeTask/Models/ImageContext.cs
+++ OrderezeTask/Models/ImageContext.cs
@@ -7,7 +7,10 @@ using System.Web;
 namespace OrderezeTask.Models
 {
     public class ImageContext:DbContext
-    {         
+    {
+        //Find Connection String
+        public ImageContext() : base("name = ImageContext") { }
+
         public DbSet<Image> Images { get; set; }
     }
 }
